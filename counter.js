389982213c6efd6a3288d36a596514e40b582990
Michael Houmann <mhoumann87@gmail.com>
2022-01-26 12:10:19 +0100

    Update HTML, CSS and JS to finished app

--- counter.js
+++ counter.js
@@ -1,7 +1,19 @@
 let count = 0;
+const countEl = document.getElementById('count-el');
+const saveEl = document.getElementById('save-el');
 
 const increment = () => {
-  count++;
+  count += 1;
 
-  document.getElementById('count-el').innerText = count;
+  countEl.textContent = count;
+};
+
+const save = () => {
+  let savedCount = count + ' - ';
+
+  saveEl.textContent += savedCount;
+
+  count = 0;
+
+  countEl.textContent = count;
 };
